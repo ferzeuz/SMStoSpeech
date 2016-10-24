8676db33877eb91006d5166771f694a96d9ff3a6
ferzeuz <teerapong454@gmail.com>
2016-10-24 17:27:10 +0700

    Update speech_test.py

--- speech_test.py
+++ speech_test.py
@@ -1,27 +1,6 @@
 #!/usr/bin/python
-
+from espeak import espeak
 import urllib, pycurl, os, urllib.parse
 
-def downloadFile(url, fileName):
-    fp = open(fileName, "wb")
-    curl = pycurl.Curl()
-    curl.setopt(pycurl.URL, url)
-    curl.setopt(pycurl.WRITEDATA, fp)
-    curl.perform()
-    curl.close()
-    fp.close()
-
-def getGoogleSpeechURL(phrase):
-    googleTranslateURL = "http://translate.google.com/translate_tts?tl=en&"
-    parameters = {'q': phrase}
-    data = urllib.parse.urlencode(parameters)
-    googleTranslateURL = "%s%s" % (googleTranslateURL,data)
-    return googleTranslateURL
-
-def speakSpeechFromText(phrase):
-    googleSpeechURL = getGoogleSpeechURL(phrase)
-    downloadFile(googleSpeechURL,"tts.mp3")
-    os.system("mplayer tts.mp3 -af extrastereo=0 &")
-   
-speakSpeechFromText("testing, testing, 1 2 3.")
+espeak.synth("Hello World Raspberry pi My name is Kala")
 
