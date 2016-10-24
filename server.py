fbbe27ff7f144e3b3d69387b8c3c3e2590148f51
ferzeuz <teerapong454@gmail.com>
2016-10-24 22:16:41 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,17 @@
-#!/usr/bin/python
-
 from flask import Flask, request, redirect
-from espeak import espeak
 import twilio.twiml
 import urllib, pycurl, os
 
 def speakSpeechFromText(phrase):
 	espeak.synth(phrase)
+print ("Espeak on")
 app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
         """Respond to incoming calls with a simple text message."""
         sms = request.args.get('Body')
-	if not sms == "":
+	
+        if not sms == "":
                 speakSpeechFromText(sms)
         resp = twilio.twiml.Response()
         return str(resp)
@@ -20,6 +19,3 @@ def hello_monkey():
 if __name__ == "__main__":
 	print ("Hello twilio")
 app.run( host='0.0.0.0', debug=True, port = 80)
-
-
-
