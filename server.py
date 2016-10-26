12db36ef209f6a2313e19574bea5f642aaa317db
ferzeuz <teerapong454@gmail.com>
2016-10-26 18:29:21 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, redirect
 from espeak import espeak
+from .espeak import ESpeak
 import twilio.twiml
 import urllib, pycurl, os
+import collections
+import re
+import subprocess
 
 class ESpeak:
-	 def __init__(self, amplitude=100, word_gap=10, capitals=1, line_length=1,
-                 pitch=50, speed=80, voice='en', spell_punctuation=[],
+	 def __init__(self, amplitude=80, word_gap=5, capitals=1, line_length=1,
+                 pitch=50, speed=30, voice='en', spell_punctuation=[],
                  split=''):
        		args = [('amplitude',         ['-a', amplitude, int]),
                 ('word_gap',          ['-g', word_gap, int]),
@@ -21,16 +25,86 @@ class ESpeak:
 
 def getPhrase(phrase):
 	textPhrase = ""
-	parameters = {'': phrase}
-	data = urllib.urlencode(parameters)
+	parameters = {"": phrase}
+	data = str.encode(parameters)
 	textPhrase = "%s%s" % (textPhrase,data)
 	return textPhrase
-	print ("getPhrase on")
 
 def speakSpeechFromText(phrase):
 	phrase = getPhrase(phrase)
 	espeak.synth(phrase)
-	print ("Espeak on")
+	
+@property
+def amplitude(self):
+        return self.args['amplitude']
+
+@amplitude.setter
+def amplitude(self, v):
+        self.args['amplitude'][1] = v
+
+@property
+def word_gap(self):
+        return self.args['word_gap']
+
+@word_gap.setter
+def word_gap(self, v):
+        self.args['word_gap'][1] = v
+
+@property
+def capitals(self):
+        return self.args['capitals']
+
+@capitals.setter
+def capitals(self, v):
+        self.args['capitals'][1] = v
+
+@property
+def line_length(self):
+        return self.args['line_length']
+
+@line_length.setter
+def line_length(self, v):
+        self.args['line_lenght'][1] = v
+
+@property
+def pitch(self):
+        return self.args['pitch']
+
+@pitch.setter
+def pitch(self, v):
+        self.args['pitch'][1] = v
+
+@property
+def speed(self):
+        return self.args['speed']
+
+@speed.setter
+def speed(self, v):
+        self.args['speed'][1] = v
+
+@property
+def voice(self):
+        return self.args['voice']
+
+@voice.setter
+def voice(self, v):
+        self.args['voice'][1] = v
+
+@property
+def spell_punctuation(self):
+        return self.args['spell_punctuation']
+
+@spell_punctuation.setter
+def spell_punctuation(self, v):
+        self.args['spell_punctuation'][1] = v
+
+@property
+def split(self):
+        return self.args['split']
+
+@split.setter
+def split(self, v):
+        self.args['split'][1] = v
 
 app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
