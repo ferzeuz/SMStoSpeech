78a04c75b9a83f3fc7ff608051f9f2dc02413d49
ferzeuz <teerapong454@gmail.com>
2016-10-28 16:29:06 +0700

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,30 @@ from flask import Flask, request, redirect
 from espeak import espeak
 import twilio.twiml
 import urllib, pycurl, os
+import collections
+import re
+import subprocess
+
+class ESpeak:
+    def __init__(self, amplitude=100, word_gap=10, capitals=1, line_length=1,
+                 pitch=50, speed=50, voice='en', spell_punctuation=[],
+                 split=''):
+        """
+        The base ESpeak class. You can set espeak's arguments from the
+        named parameters or change them after creation using the properties
+        """
+
+        args = [('amplitude',         ['-a', amplitude, int]),
+                ('word_gap',          ['-g', word_gap, int]),
+                ('capitals',          ['-k', capitals, int]),
+                ('line_length',       ['-l', line_length, int]),
+                ('pitch',             ['-p', pitch, int]),
+                ('speed',             ['-s', speed, int]),
+                ('voice',             ['-v', voice, str]),
+                ('spell_punctuation', ['--punct=', ''.join(spell_punctuation),
+                                       str]),
+                ('split',             ['--split=', split, str])]
+        self.args = collections.OrderedDict(args)
 
 def getPhrase(phrase):
 	textPhrase = ""
@@ -12,7 +36,7 @@ def getPhrase(phrase):
 
 def speakSpeechFromText(phrase):
 	phrase = getPhrase(phrase)
-	espeak.synth(phrase)
+	espeak.synth(phrase) -s100
 	print("Espeak on")
 app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
